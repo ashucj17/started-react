e274818524892b95449f7bb752ce738c153cc6e6
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2024-12-03 20:58:47 +0530

    fist component in react

---
+++ src/MyFun.jsx
@@ -0,0 +1,9 @@
+function MyFun() {
+  return (
+    <div>
+      <h2>Let start with react</h2>
+    </div>
+  );
+}
+
+export default MyFun;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+import MyFun from "./MyFun";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <MyFun />
   </React.StrictMode>
 );
